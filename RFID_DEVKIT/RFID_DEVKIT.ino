4a8b58562ef21383cb556c1e444861098b93859f
FranklinMolle <80933861+FranklinMolle@users.noreply.github.com>
2021-10-09 09:12:15 -0500

    Update RFID_DEVKIT.ino

--- RFID_DEVKIT/RFID_DEVKIT.ino
+++ RFID_DEVKIT/RFID_DEVKIT.ino
@@ -5,46 +5,44 @@
 #define RXLED  17
 
 Ufox wisol;
-MFRC522 mfrc522(0x28, 4);     //CREAMOS EL OBJETO "mfrc522" Y EL PIN DE RESET '4'  
-String StringID = "";         //CREAMOS EL STRING PARA EL DATO LEIDO
-uint32_t valor = 0 ;          //CREAMOS EL ENTERO PARA EL DATO LEIDO
+MFRC522 mfrc522(0x28, 4);     //Creamos el objeto "mfrc522" y el pin 4 para el reset del i2c  
+String StringID = "";         //creamos un string para el dato leido
+uint32_t valor = 0 ;          //creamos el entero para que posteriormente convertir un string a entero
 
 void setup() {
   Wire.begin(); 
   pinMode(RXLED,OUTPUT);
   Serial.begin(9600);          
-  mfrc522.PCD_Init();         //INICIALIZA EL MODULO
+  mfrc522.PCD_Init();         //inicializa el modulo
   wisol.begin(9600);            
 }
 
 void loop() {
-  /* PREGUNTAMOS SI HAY UNA TARJETA O LLAVERO PRESENTE Y SI SE PUEDE OBETNER LOS DATOS
-     RETORNA AL LOOP*/ 
+  /* preguntamos si hay una tarjeta o llavero presente y si se pude obtener los datos en caso sea falso retorar al loop */
   if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
     delay(50);
     return;
   }
-  StringID = "";              //LIMPIAMOS EL VALOR DEL STRING
+  StringID = "";              //limpiamos el valor del string
   /*************** OBTENEMOS EL VALOR DEL UID DE LA TARJETA ***********/
   Serial.print(F("Card UID:"));
   for (byte i = 0; i < mfrc522.uid.size; i++) {
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     Serial.print(mfrc522.uid.uidByte[i], DEC);
     StringID = StringID + String(mfrc522.uid.uidByte[i], DEC);
-    valor = StringID.toInt();
+    valor = StringID.toInt();   //conevrtimos el valor(string) de la tarjeta a un valor entero para el envio del sigfox
   } 
-  /********************************************************************/
+  /***************  ****************************************************/
   Serial.println();
-  //Serial.println(StringID);
-  Serial.println(valor);
-  mfrc522.PICC_HaltA();       //DETENEMOS LA COMUNICACION DE LA TARJETA
+  Serial.println(valor);      
+  mfrc522.PICC_HaltA();       //detenemos la comunicacion del modulo
   //**************** ENVIO POR SIGFOX ***********
   digitalWrite(RXLED,LOW);
-  wisol.RST();                        //REINICIA O DESPIERTA AL WISOL
-  Serial.println(wisol.SEND(valor));  //ENVIAMOS EL VALOR OBTENIDO
+  wisol.RST();                        //reinicia y despierta al modulo
+  Serial.println(wisol.SEND(valor));  //enviamos el valor obtenido
   digitalWrite(RXLED,HIGH);
-  //wisol.SLEEP();                      //MODO SLEEP
+  wisol.SLEEP();                      //modo sleep
   delay(3000);
-  Serial.println("Enviado");          //ENVIAMOS UN MENSAJE DE CONFIRMACION 
+  Serial.println("Enviado");          //enviamos un mensaje de confirmacion 
   //************
 }
